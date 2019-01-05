195d990d8687763341bc263472307866bbb96d91
heihei <13723776735@163.com>
2019-01-05 19:26:43 +0800

    在自选股处选择取消选中自选股

--- pages/like/like.js
+++ pages/like/like.js
@@ -5,17 +5,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    pagejump: true,
+    Code: []
   },
 
   /* 处理选中的股票 */
   touch_choose: function (e) {
-    var id = e.currentTarget.dataset.select;
-
-    wx.setStorageSync('code', id);
-    wx.navigateTo({
-      url: '../get/get',
-    })
+    if (this.data.pagejump) {
+      var id = e.currentTarget.dataset.select;
+      wx.setStorageSync('code', id);
+      wx.navigateTo({
+        url: '../get/get',
+      })
+    }
+    this.setData({ pagejump: true });
   },
 
   /**
@@ -47,7 +50,61 @@ Page({
           Code: Code
         })
       }
+    });
+  },
+
+  /**
+   * 在自选股处取消自选股
+   */
+  delete_option:function (e) {
+    this.setData({ pagejump: false });
+    var that = this;
+    var id = e.target.id;
+    var choose = this.data.Code;
+
+    var code = '"' + e.target.id + '"';
+    var openid = '"' + app.useropenID + '"';
+    var index = -1;
+    for (var i = 0; i < choose.length; i++) {
+      if (id == choose[i].code) {
+        index = i;
+        break;
+      }
+    }
+    if(index == -1) {
+      return;
+    }
+    wx.request({
+      url: 'https://www.szu522.cn:50003/addstock.php',
+      data: {
+        openid: openid,
+        code: code
+      },
+      dataType: 'json',
+      method: 'GET',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        var result = res.data;
+        if (result.resultCode == 200) {
+          wx.showToast({
+            title: result.desc,
+          })
+        } else {
+          // 输出错误提示
+          that.setData({
+            popErrorMsg: result.desc
+          });
+          that.fadeOut();
+        }
+        that.setData({
+          Code: that.data.Code
+        })
+        that.onLoad();
+      }
     })
+    
   },
 
   /**
